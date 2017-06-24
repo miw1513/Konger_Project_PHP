6c31cc8a45d20a0df0ad89e2b7cf8c7dfb7e4f83
miw1513 <miw1513@hotmail.com>
2017-06-25 00:01:05 +0700

    updated
    updated

---
+++ Truemoney.php
@@ -0,0 +1,4 @@
+
+ชื่อ : <input type="text" name="ref2" > <br>
+รหัสบัตรทรู : <input type="text" name="ref1" > <br>
+ข้อความ : <textarea> </textarea>
---
+++ index.php
@@ -0,0 +1,27 @@
+<!DOCTYPE html>
+<html lang="en">
+    <head>
+        <title></title>
+        <meta charset="UTF-8">
+        <meta name="viewport" content="width=device-width, initial-scale=1">
+        <link href="css/style.css" rel="stylesheet">
+    </head>
+    <body>
+    
+
+    Konger
+    Donate
+    <ul>
+        <li>ทรูมันนี่</li>
+        <li>โอนธนาคาร</li>
+        <li>ทรูวอลเลต</li>
+        <li>Paypal</li>
+
+
+        
+        <button type="button"></button>
+    </ul>
+
+
+    </body>
+</html>
